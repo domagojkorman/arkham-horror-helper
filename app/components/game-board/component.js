cb4a65eadbd15d141b988b8104077aa91f036437
Domagoj Korman <domagojkorman93@gmail.com>
2021-02-27 19:00:35 +0100

    Implement token bag functions

--- app/components/game-board/component.js
+++ app/components/game-board/component.js
@@ -2,27 +2,66 @@ import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
 import cards from 'arkham-horror-helper/models/cards';
-import {A as emberArray } from '@ember/array';
 import CardModel from 'arkham-horror-helper/models/card';
+import {randomItem} from 'arkham-horror-helper/utils/shuffle';
 
 const Mode = {
-  IN_GAME: 1,
-  EDIT_TOKENS: 2,
-  EDIT_DECK: 3,
-  EDIT_PLAY_AREA: 4,
+  IN_GAME: 'game',
+  EDIT_TOKENS: 'tokens',
+  EDIT_DECK: 'deck',
+  EDIT_PLAY_AREA: 'play',
 }
 export default class GameBoardComponent extends Component {
 
   @tracked tokens = []
+  @tracked drawnTokens = []
+  @tracked tokenDrawCount = 0
+  @tracked cards = []
   @tracked deck = []
+  @tracked playDeck = []
+  @tracked discardDeck = []
   @tracked mode = Mode.IN_GAME
 
   get isEditMode() {
     return this.mode !== Mode.IN_GAME
   }
 
+
+  // TOKENS
+  get tokenBag() {
+    return this.tokens.filter((token) => !this.drawnTokens.includes(token));
+  }
+
   get isEditTokensMode() {
     return this.mode === Mode.EDIT_TOKENS;
+  }
+
+  get canDrawToken() {
+    return this.tokens.length;
+  }
+
+  get canDrawNextToken() {
+    return this.tokenBag.length;
+  }
+
+  @action onDrawTokenClick() {
+    this.drawnTokens = [];
+    this.onDrawNextTokenClick();
+  }
+
+  @action onDrawNextTokenClick() {
+    const token = randomItem(this.tokenBag);
+    this.tokenDrawCount = this.tokenDrawCount + 1;
+    token.drawCount = this.tokenDrawCount;
+    this.drawnTokens = [...this.drawnTokens, token];
+  }
+
+  @action onTokenClick(token) {
+    this.drawnTokens = this.drawnTokens.filter((t) => t !== token);
+  }
+
+  @action setMode(mode) {
+    this.mode = mode;
   }
 
   @action onEditTokensClick() {
--- app/components/play-area/component.js
+++
@@ -1,4 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class PlayAreaComponent extends Component {
-}
